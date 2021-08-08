7ca6dace3cc46faf9ab4aacf717ea70c147ab2c9
Andrew Dawson <dawso.andrew@gmail.com>
2021-08-08 01:38:30 -0400

    Updates the text fields and removes form control and label components.

--- react-app/src/Components/LoginForm/LoginForm.tsx
+++ react-app/src/Components/LoginForm/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { Button } from "Components/Button";
-import { FormControl } from "Components/FormControl";
+import { FieldError } from "Components/FieldError";
 import { TextField } from "Components/TextField";
 import React from "react";
 import { useForm } from "react-hook-form";
@@ -7,6 +7,7 @@ import { useTranslation } from "react-i18next";
 import { createId } from "Utilities/HtmlIdAttribute";
 
 export interface LoginFormData {
+  password: string;
   username: string;
 }
 
@@ -22,21 +23,33 @@ export const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
     formState: { errors },
   } = useForm<LoginFormData>();
 
-  const usernameInputId = "username";
-  const usernameErrorId = createId(usernameInputId, "error");
+  const passwordFieldId = "password";
+  const passwordErrorId = createId(passwordFieldId, "error");
+  const usernameFieldId = "username";
+  const usernameErrorId = createId(usernameFieldId, "error");
 
   return (
-    <form noValidate onSubmit={handleSubmit(onSubmit)}>
-      <FormControl
-        errorId={usernameErrorId}
-        inputId={usernameInputId}
-        label={t("login_form.username_field_label")}
-      >
+    <form className="flex flex-col gap-3" noValidate onSubmit={handleSubmit(onSubmit)}>
+      <div>
         <TextField
           errorId={usernameErrorId}
-          inputProps={{ id: usernameInputId, ...register("username") }}
+          id={usernameFieldId}
+          inputProps={{ ...register("username") }}
+          label={t("login_form.username_field_label")}
+          name="username"
         />
-      </FormControl>
+        <FieldError error={errors.username?.message} />
+      </div>
+      <div>
+        <TextField
+          errorId={passwordErrorId}
+          id={passwordFieldId}
+          inputProps={{ ...register("password") }}
+          label={t("login_form.password_field_label")}
+          name="password"
+        />
+        <FieldError error={errors.password?.message} />
+      </div>
       <Button type="submit">{t("login_form.login_button_label")}</Button>
     </form>
   );
--- react-app/src/Components/TextField/TextField.tsx
+++ react-app/src/Components/TextField/TextField.tsx
@@ -1,107 +1,72 @@
 import clsx from "clsx";
 import React, {
-  ChangeEventHandler,
   DetailedHTMLProps,
-  FocusEventHandler,
   forwardRef,
   InputHTMLAttributes,
-  ReactNode,
 } from "react";
-import { useState } from "react";
-import { joinIds } from "Utilities/HtmlIdAttribute";
+import { createId, joinIds } from "Utilities/HtmlIdAttribute";
 
-type InputType = "email" | "password" | "search" | "text";
-
-export interface TextFieldProps {
+interface TextFieldProps {
   className?: string;
-  endInsert?: ReactNode;
   errorId?: string;
   hasError?: boolean;
   helpId?: string;
-  id?: string;
+  id: string;
   inputProps?: DetailedHTMLProps<
     InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
   >;
   isDisabled?: boolean;
   isRequired?: boolean;
-  maxLength?: number;
-  name?: string;
-  onChange?: ChangeEventHandler<HTMLInputElement>;
+  label: string;
+  name: string;
   placeholder?: string;
-  startInsert?: ReactNode;
-  type?: InputType;
-  value?: string;
 }
 
 export const TextField = forwardRef<HTMLDivElement, TextFieldProps>(
   (
     {
       className,
-      endInsert,
       errorId,
       hasError,
       helpId,
       id,
-      inputProps = {},
+      inputProps,
       isDisabled,
       isRequired,
-      maxLength,
+      label,
       name,
-      onChange,
       placeholder,
-      startInsert,
-      type = "text",
-      value,
     },
     ref
   ) => {
-    const { onBlur, ...otherInputProps } = inputProps;
-    const [isFocused, setIsFocused] = useState(false);
-
-    const handleBlur: FocusEventHandler<HTMLInputElement> = (event) => {
-      setIsFocused(false);
-      if (onBlur) {
-        onBlur(event);
-      }
-    };
-
-    const handleFocus = () => {
-      setIsFocused(true);
-    };
+    const inputId = createId(id, "input");
 
     return (
       <div
-        // className={clsx(
-        //   styles.container,
-        //   isDisabled && styles.containerDisabled,
-        //   isFocused && styles.containerFocused,
-        //   hasError && styles.containerError,
-        //   hasError && !isFocused && styles.containerErrorBlurred
-        // )}
+        className={clsx(
+          "border border-gray-500 flex-col focus-within:border-focus focus-within:ring-2 focus-within:ring-focus rounded",
+          className
+        )}
+        id={id}
         ref={ref}
       >
-        {startInsert}
+        <label className="block px-2 pt-1" htmlFor={inputId}>
+          {label}
+          {isRequired && <span aria-hidden="true">&thinsp;*</span>}
+        </label>
         <input
           aria-describedby={joinIds(errorId, helpId)}
           aria-invalid={hasError}
-          autoComplete="off"
-        //   className={clsx(styles.input, className)}
+          className="block outline-none px-2 pb-1 w-full"
           disabled={isDisabled}
-          id={id}
-          maxLength={maxLength}
+          id={inputId}
           name={name}
-          onBlur={handleBlur}
-          onChange={onChange}
-          onFocus={handleFocus}
-          required={isRequired ? true : undefined}
           placeholder={placeholder}
-          spellCheck={false}
-          type={type}
-          value={value}
-          {...otherInputProps}
+          required={isRequired}
+          type="text"
+          {...inputProps}
         />
-        {endInsert}
       </div>
     );
   }
